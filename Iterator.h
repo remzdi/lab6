86e9a5177cd7cf3d392885b8a1bf7ea53c8e7201
remzdi <116484641+remzdi@users.noreply.github.com>
2023-06-20 15:10:52 +0300

    Create Iterator.h

---
+++ Iterator.h
@@ -0,0 +1,39 @@
+#ifndef ITERATOR_H
+#define ITERATOR_H
+
+template<typename KeyType, typename ValueType>
+class LinkedListAssociativeArray;
+using namespace std;
+
+template<typename KeyType, typename ValueType>
+class LinkedListAssociativeArrayIterator {
+private:
+    typename LinkedListAssociativeArray<KeyType, ValueType>::Node* current;
+
+public:
+    LinkedListAssociativeArrayIterator(typename LinkedListAssociativeArray<KeyType, ValueType>::Node* node) : current(node) {}
+
+    bool operator==(const LinkedListAssociativeArrayIterator& other) const {
+        return current == other.current;
+    }
+
+    bool operator!=(const LinkedListAssociativeArrayIterator& other) const {
+        return !(*this == other);
+    }
+
+    LinkedListAssociativeArrayIterator& operator++() {
+        if (current) {
+            current = current->next;
+        }
+        return *this;
+    }
+
+    ValueType& operator*() const {
+        if (!current) {
+            throw out_of_range("Iterator is out of range.");
+        }
+        return current->value;
+    }
+};
+
+#endif
