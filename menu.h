1188630625e8fda2f380cabfa0cb9abb71e3f2f8
remzdi <116484641+remzdi@users.noreply.github.com>
2023-08-28 09:04:42 +0300

    Update menu.h

--- menu.h
+++ menu.h
@@ -1,2 +1,4 @@
 #include "LinkedListAssociativeArray.h"
 void runArrayOperations(LinkedListAssociativeArray<int, string>& array);
+void runArrayOperations(LinkedListAssociativeArray<string, double>& array);
+void runArrayOperations(LinkedListAssociativeArray<string, string>& array);
