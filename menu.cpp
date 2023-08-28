78744ff9b020decc9e2625f7f629e8d73f7f07ff
remzdi <116484641+remzdi@users.noreply.github.com>
2023-08-28 09:04:23 +0300

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -81,5 +81,174 @@ void runArrayOperations(LinkedListAssociativeArray<int, string>& array) {
 
         cout << endl;
 
-    } while (choice != 0);
+    } while (choice != 0 );
 }
+
+void runArrayOperations(LinkedListAssociativeArray<string, double>& array) {
+    int choice;
+
+    do {
+        cout << "1. Add key-value pair" << endl;
+        cout << "2. Remove key-value pair" << endl;
+        cout << "3. Find minimum key-value pair" << endl;
+        cout << "4. Clear array" << endl;
+        cout << "5. Print array elements" << endl;
+        cout << "6. Is array empty?" << endl;
+        cout << "0. Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1: {
+                string key;
+                double value;
+                cout << "Enter key: ";
+                cin >> key;
+                cout << "Enter value: ";
+                cin >> value;
+                array.insert(key, value);
+                cout << "Key-value pair added successfully." << endl;
+                break;
+            }
+            case 2: {
+                string key;
+                cout << "Enter key to remove: ";
+                cin >> key;
+                if (array.remove(key)) {
+                    cout << "Key-value pair removed successfully." << endl;
+                } else {
+                    cout << "Key not found in the array." << endl;
+                }
+                break;
+            }
+            case 3: {
+                string key;
+                double value;
+                if (array.findMinimum(key, value)) {
+                    cout << "Minimum key-value pair: Key=" << key << ", Value=" << value << endl;
+                } else {
+                    cout << "Array is empty." << endl;
+                }
+                break;
+            }
+            case 4: {
+                array.clear();
+                cout << "Array cleared." << endl;
+                break;
+            }
+            case 5: {
+                cout << "Array elements: ";
+                bool isFirstElement = true;
+                for (typename LinkedListAssociativeArray<string, double>::Iterator it = array.begin(); it != array.end(); ++it) {
+                    if (!isFirstElement) {
+                        cout << ", ";
+                    }
+                    cout << *it;
+                    isFirstElement = false;
+                }
+                cout << endl;
+                break;
+            }
+            case 6: {
+                if (array.isEmpty()) {
+                    cout << "Array is empty." << endl;
+                } else {
+                    cout << "Array is not empty." << endl;
+                }
+                break;
+            }
+            case 0:
+                break;
+            default:
+                cout << "Invalid choice. Please try again." << endl;
+        }
+
+        cout << endl;
+
+    } while (choice != 0 );
+}
+
+void runArrayOperations(LinkedListAssociativeArray<string, string>& array) {
+    int choice;
+
+    do {
+        cout << "1. Add key-value pair" << endl;
+        cout << "2. Remove key-value pair" << endl;
+        cout << "3. Find minimum key-value pair" << endl;
+        cout << "4. Clear array" << endl;
+        cout << "5. Print array elements" << endl;
+        cout << "6. Is array empty?" << endl;
+        cout << "0. Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1: {
+                string key;
+                string value;
+                cout << "Enter key: ";
+                cin >> key;
+                cout << "Enter value: ";
+                cin >> value;
+                array.insert(key, value);
+                cout << "Key-value pair added successfully." << endl;
+                break;
+            }
+            case 2: {
+                string key;
+                cout << "Enter key to remove: ";
+                cin >> key;
+                if (array.remove(key)) {
+                    cout << "Key-value pair removed successfully." << endl;
+                } else {
+                    cout << "Key not found in the array." << endl;
+                }
+                break;
+            }
+            case 3: {
+                string key;
+                string value;
+                if (array.findMinimum(key, value)) {
+                    cout << "Minimum key-value pair: Key=" << key << ", Value=" << value << endl;
+                } else {
+                    cout << "Array is empty." << endl;
+                }
+                break;
+            }
+            case 4: {
+                array.clear();
+                cout << "Array cleared." << endl;
+                break;
+            }
+            case 5: {
+                cout << "Array elements: ";
+                bool isFirstElement = true;
+                for (LinkedListAssociativeArray<string, string>::Iterator it = array.begin(); it != array.end(); ++it) {
+                    if (!isFirstElement) {
+                        cout << ", ";
+                    }
+                    cout << *it;
+                    isFirstElement = false;
+                }
+                cout << endl;
+                break;
+            }
+            case 6: {
+                if (array.isEmpty()) {
+                    cout << "Array is empty." << endl;
+                } else {
+                    cout << "Array is not empty." << endl;
+                }
+                break;
+            }
+            case 0:
+                break;
+            default:
+                cout << "Invalid choice. Please try again." << endl;
+        }
+
+        cout << endl;
+
+    } while (choice != 0 );
+}
+
