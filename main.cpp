92c8ae9aaad1a692a1ea0e6e91fadd3d17b5f1e0
remzdi <116484641+remzdi@users.noreply.github.com>
2023-08-28 09:03:57 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,33 @@
 #include "menu.h"
 
 int main() {
-    LinkedListAssociativeArray<int, string> array;
-    runArrayOperations(array);
+    int choice;
+    cout << "Choose the types for key and value:\n";
+    cout << "1. int, string\n";
+    cout << "2. string, double\n";
+    cout << "3. string, string\n";
+    cout << "Enter your choice: ";
+    cin >> choice;
+
+    switch (choice) {
+        case 1: {
+            LinkedListAssociativeArray<int, string> array;
+            runArrayOperations(array);
+            break;
+        }
+        case 2: {
+            LinkedListAssociativeArray<string, double> array;
+            runArrayOperations(array);
+            break;
+        }
+        case 3: {
+            LinkedListAssociativeArray<string, string> array;
+            runArrayOperations(array);
+            break;
+        }
+        default:
+            cout << "Invalid choice." << endl;
+            break;
+}
     return 0;
 }
